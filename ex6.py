8bc26b45ecd455da1ba219b0840a49a15f688fee
emillydiniz09 <emilly.weingartner@escola.pr.gov.br>
2026-06-19 11:57:15 +0000

    versao1

---
+++ ex6.py
@@ -0,0 +1,9 @@
+nota = float(input("Digite a nota: "))
+if nota >= 9:
+    print("Conceito A")
+elif nota >= 7: 
+    print("Conceito B") 
+elif nota >= 5:
+      print("Conceito C")
+else:
+     print("conceito D")
