3e967e4f0e5a772a04a6c318a24d13622756edab
emillydiniz09 <emilly.weingartner@escola.pr.gov.br>
2026-06-26 11:51:00 +0000

    versao2

---
+++ ex14.py
@@ -0,0 +1,7 @@
+idade = int(input("Idade: "))
+altura = float(input("Altura (m): "))
+if idade >= 12 and altura >= 1.40:
+    print("Pode entrar no brinquedo")
+else:
+    print("Não pode entrar")
+    
